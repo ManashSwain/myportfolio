f9e49cdc5a83ebe482508c9b2d91343adf4f14b3
ManashSwain <manash0802@gmail.com>
2025-01-13 22:53:28 +0530

    updated education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,35 +1,28 @@
 const Education = () => {
   return (
   <>
-  <h2>Education</h2>
+  <h2 className="text-xl font-bold mt-6">Education</h2>
    <div>
    <ul role="list" className="divide-y divide-gray-100">
   <li className="flex justify-between gap-x-6 py-5">
     <div className="flex min-w-0 gap-x-4">
-      <img className="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+    <img
+                className="size-12 w-12 h-12 flex-none rounded-full bg-gray-50 object-cover"
+                src="public/Anna_University_Logo.svg.png"
+                alt="annauniversity-image"
+              />
       <div className="min-w-0 flex-auto">
-        <p className="text-sm/6 font-semibold text-gray-900">Leslie Alexander</p>
-        <p className="mt-1 truncate text-xs/5 text-gray-500">leslie.alexander@example.com</p>
+        <p className="text-sm/6 font-semibold text-gray-900">Anna University</p>
+        <p className="mt-1 truncate text-xs/5 text-gray-500"> </p>
+        <p className="mt-1 truncate text-xs/5 text-gray-500">B.E - Bachelor of Engineering </p>
       </div>
     </div>
     <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p className="text-sm/6 text-gray-900">Co-Founder / CEO</p>
-      <p className="mt-1 text-xs/5 text-gray-500">Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time></p>
+      <p className="text-sm/6 text-gray-900">Aug 2016 - Oct 2020</p>
+      
     </div>
   </li>
-  <li className="flex justify-between gap-x-6 py-5">
-    <div className="flex min-w-0 gap-x-4">
-      <img className="size-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-      <div className="min-w-0 flex-auto">
-        <p className="text-sm/6 font-semibold text-gray-900">Michael Foster</p>
-        <p className="mt-1 truncate text-xs/5 text-gray-500">michael.foster@example.com</p>
-      </div>
-    </div>
-    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-      <p className="text-sm/6 text-gray-900">Co-Founder / CTO</p>
-      <p className="mt-1 text-xs/5 text-gray-500">Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time></p>
-    </div>
-  </li>
+  
 </ul>
    </div>
   </>
