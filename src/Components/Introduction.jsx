dc781028bf8f75c55a4321d473dcf738ceca545d
ManashSwain <manash0802@gmail.com>
2025-01-13 16:15:06 +0530

    completed introduction section structure]

---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+const Introduction = () => {
+  return (
+    <>
+    <div className="introduction">
+    <div className="intro">
+     <h1>Hi, I'm Manash 👋</h1>
+     <p>Software Engineer turned Freelancer. I love building things and helping people. Very active on LinkedIn.</p>
+    </div>
+    <div className="profile-image">
+          <img src="" alt="profile-image" />
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Introduction
