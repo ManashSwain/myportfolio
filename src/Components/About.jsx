961f9a1061f4f96d8295856378f4518e57f9dd46
ManashSwain <manash0802@gmail.com>
2025-01-13 16:30:27 +0530

    fixed typos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,11 +3,11 @@ const About = () => {
    <>
    <h2>About Me</h2>
    <h3>
-   Hi there! 👋 I'm Manash Swain, a passionate MERN Stack Developer with over 4 years of experience delivering impactful web solutions. I specialize in creating modern, responsive, and user-friendly applications that solve real-world problems.
+   Hi there! 👋 I’m Manash Swain, a passionate MERN Stack Developer with over 4 years of experience delivering impactful web solutions. I specialize in creating modern, responsive, and user-friendly applications that solve real-world problems.
 
    Previously, I worked at Cognizant Technology Solutions, where I contributed to projects for leading clients like Nike, gaining hands-on experience in building scalable applications and collaborating with cross-functional teams.
 
-   When I'm not coding, I enjoy exploring new technologies, working on personal projects, and sharing my knowledge with others. I’m always eager to learn and grow as a developer, embracing challenges as opportunities to innovate and improve.
+   When I’m not coding, I enjoy exploring new technologies, working on personal projects, and sharing my knowledge with others. I’m always eager to learn and grow as a developer, embracing challenges as opportunities to innovate and improve.
 
    Feel free to explore my portfolio to see my skills, projects, and experience in action. Let’s connect and create something amazing together! 🚀
    </h3>
