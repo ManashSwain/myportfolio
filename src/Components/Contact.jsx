88616674abd0a3331fd547e3d3657c3a831fe397
ManashSwain <manash0802@gmail.com>
2025-01-13 18:21:13 +0530

    added contact session

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <>
+    <button  className="rounded-md bg-slate-800 py-1 px-2 border border-transparent text-center text-xs text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2" type="button">Contact</button>
+    <h2>Get in Touch</h2>
+    <p>Want to chat? Just give me a dm with a direct question on LinkedIn and I will respond whenever I can.</p>
+    </>
+  )
+}
+
+export default Contact
