46548fbe1f1eeb79e389e6dfaa6782019e47cda1
ManashSwain <manash0802@gmail.com>
2025-01-14 22:24:24 +0530

    ui mods

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,7 +2,20 @@
 const Projects = () => {
   return (
     <>
-    <div></div>
+    <div>
+    <div className="text-center my-12">
+        <button
+          className="rounded-md bg-slate-800 py-1 m-1 px-2 border border-transparent text-center text-xs text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+          type="button"
+        >
+          All Projects
+        </button>
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl my-2">
+        Explore all my projects here
+        </h2>
+        
+      </div>
+    </div>
     </>
   )
 }
